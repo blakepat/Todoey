075a48de8ac0a23d281d66e70ea9670e729b2521
Blake Pat <blakepat@me.com>
2019-06-03 18:05:35 -0400

    Save Data onto pList (local data storage working)

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -12,11 +12,16 @@ class ToDoListViewController: UITableViewController {
 
     var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     
-    
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
+        
     }
 
     // MARK - Tableview Datasource Methods
@@ -68,6 +73,11 @@ class ToDoListViewController: UITableViewController {
             //what will happen once the user clicks the add item button on UIAlert
             
             self.itemArray.append(newItem.text!)
+            
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
+            
             self.tableView.reloadData()
         }
         
